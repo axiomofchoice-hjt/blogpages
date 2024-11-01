d9dea7d417530ac22e2b2bb783fe9a59a13b89e8
axiomofchoice-hjt <Axiomofchoice@163.com>
2024-11-01 20:43:11 +0800

    =

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -97,7 +97,7 @@ module.exports = {
       { text: "首页", link: "/" },
       { text: '分类', link: '/categories/' },
       { text: '归档', link: '/archives/' },
-      { text: "Blockchallenge", link: '/blockchallenge/' },
+      { text: "puzzles", link: '/puzzles/' },
       { text: "败犬日报", link: 'https://underdog-daily.pages.dev/' }
     ],
     updateBar: { showToArticle: false, moreArticle: '/archives/' }
